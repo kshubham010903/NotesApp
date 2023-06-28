c7f0c644167ba762ff09529f12dec8aafe761191
Sachin Desai <sachind@segoma.com>
2023-06-28 17:56:37 +0530

    first commit

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
+# Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+    return Response('Our Api')
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
